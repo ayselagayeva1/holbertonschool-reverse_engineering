01414b9f327af76db53a9ca834abc0e54f369290
elvinmammadzada <elvinmammadzada643@gmail.com>
2026-07-27 14:56:52 +0400

    Update get_entry_point.sh

--- re_fundamentals/get_entry_point.sh
+++ re_fundamentals/get_entry_point.sh
@@ -43,13 +43,14 @@ fi
 # --- 4. Extract required ELF header fields using readelf ---
 
 # Magic Number: first line of readelf -h, e.g. "7f 45 4c 46 02 01 01 00 ..."
-magic_number=$(readelf -h "$file_name" | grep "Magic:" | sed 's/.*Magic:[[:space:]]*//')
+magic_number=$(readelf -h "$file_name" | grep "Magic:" | sed 's/.*Magic:[[:space:]]*//' | sed 's/[[:space:]]*$//')
 
 # Class: e.g. "ELF64" or "ELF32"
 class=$(readelf -h "$file_name" | grep "Class:" | awk '{print $2}')
 
-# Byte Order (Data field): e.g. "2's complement, little endian"
-byte_order=$(readelf -h "$file_name" | grep "Data:" | sed 's/.*Data:[[:space:]]*//')
+# Byte Order (Data field): readelf shows e.g. "2's complement, little endian"
+# We only want "little endian" or "big endian"
+byte_order=$(readelf -h "$file_name" | grep "Data:" | grep -oE "little endian|big endian")
 
 # Entry Point Address: e.g. "0x1040"
 entry_point_address=$(readelf -h "$file_name" | grep "Entry point address:" | awk '{print $NF}')
